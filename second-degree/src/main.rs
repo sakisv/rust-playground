8a6eca659fa6b2a5cbea814473d3d65244ffe430
sakisv <me@sakisv.net>
2023-10-16 00:02:23 +0300

    Refactor: Make it work
    Remove the unused `structs` and `trait` and implement a `get_roots`
    function straight on the `DType` enum - because apparently that's a
    thing that can happen(!)
    
    The trickiest thing was to understand how to extract (or maybe unwrap?)
    the <i32> type out of the different DType variations.
    
    The `match` statement not only can allow for nested matches (e.g.
    `DType::DPositive`), but also can bind the matched value to a variable
    (e.g. `DType::DPositive(d)`) which allows us to use it easier in the
    relevant `match` block.

--- second-degree/src/main.rs
+++ second-degree/src/main.rs
@@ -5,16 +5,6 @@ enum DType {
     DZero(i32),
 }
 
-struct DPositive {
-    d: i32,
-}
-struct DNegative {
-    _d: i32,
-}
-struct DZero {
-    _d: i32,
-}
-
 struct SecondDegree {
     a: i32,
     b: i32,
@@ -22,40 +12,37 @@ struct SecondDegree {
     d: DType,
 }
 
-trait Roots {
-    fn get_roots(&self, a: i32, b: i32, _c: i32) -> (f64, f64);
+impl DType {
+    fn get_roots(&self, a: i32, b: i32, _c: i32) -> (f64, f64) {
+        match self {
+            DType::DPositive(d) => {
+                let res1 = (f64::from(-b) + f64::from(*d).sqrt()) / 2.0 * f64::from(a);
+                let res2 = (f64::from(-b) - f64::from(*d).sqrt()) / 2.0 * f64::from(a);
+                (res1, res2)
+            },
+            DType::DZero(_) => {
+                let res = f64::from(-b) / 2.0 * f64::from(a);
+                (res, res)
+            },
+            DType::DNegative(_) => {
+                (f64::NAN, f64::NAN)
+            }
+        }
+    }
 }
 
 impl SecondDegree {
-    fn new(&self, a: i32, b: i32, c: i32) -> Self {
+    fn new(a: i32, b: i32, c: i32) -> Self {
         let d = b * b - 4 * a * c;
         match d {
             d if d.is_positive() =>  SecondDegree{a, b, c, d: DType::DPositive(d)},
             d if d.is_negative() =>  SecondDegree{a, b, c, d: DType::DNegative(d)},
             _ =>  SecondDegree{a, b, c, d: DType::DZero(d)},
         }
-
     }
-}
 
-impl Roots for DPositive {
-    fn get_roots(&self, a: i32, b: i32, _c: i32) -> (f64, f64) {
-        let res1 = (f64::from(-b) + f64::from(self.d).sqrt()) / 2.0 * f64::from(a);
-        let res2 = (f64::from(-b) - f64::from(self.d).sqrt()) / 2.0 * f64::from(a);
-        (res1, res2)
-    }
-}
-
-impl Roots for DZero {
-    fn get_roots(&self, a: i32, b: i32, _c: i32) -> (f64, f64) {
-        let res = f64::from(-b) / 2.0 * f64::from(a);
-        (res, res)
-    }
-}
-
-impl Roots for DNegative {
-    fn get_roots(&self, _a: i32, _b: i32, _c: i32) -> (f64, f64) {
-        (f64::NAN, f64::NAN)
+    fn get_roots(&self) -> (f64, f64) {
+        self.d.get_roots(self.a, self.b, self.c)
     }
 }
 
@@ -65,5 +52,6 @@ fn main() {
 
     let sd = SecondDegree::new(a, b, c);
 
-    println!("D = {:?}", sd.d.get_roots());
+    println!("D = {:?}", sd.d);
+    println!("Roots = {:?}", sd.get_roots());
 }
